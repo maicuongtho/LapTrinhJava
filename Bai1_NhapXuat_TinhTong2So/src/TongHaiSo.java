bfc2c1d2d4b52aae3bb8b5c6de4a71b505f7caf7
Mai Cường Thọ <thomc@ntu.edu.vn>
2026-02-03 09:57:07 +0700

    Them mã nhập, khai báo biến

--- Bai1_NhapXuat_TinhTong2So/src/TongHaiSo.java
+++ Bai1_NhapXuat_TinhTong2So/src/TongHaiSo.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
 
 public class TongHaiSo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Khai báo
+		double num1, num2, sum;
+		
+		// Khai báo đối tượng Scanner
+		Scanner banPhim = new Scanner(System.in);
+		
+		// In ra thông tin, mời User nhập số thứu nhất
+		
+		System.out.print("Mời nhập số thứ nhất: ");
+		num1= banPhim.nextDouble();
+		
+		System.out.print("Mời nhập số thứ hai: ");
+		num2= banPhim.nextDouble();
+		
 
 	}
 

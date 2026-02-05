f9ec06f6d50e65599886782ad1d560a41eaf2009
Mai Cường Thọ <thomc@ntu.edu.vn>
2026-02-05 09:34:38 +0700

    Nộp lên kẻo thhaayf cho Zé rô

---
+++ Vidu1_OOP_SinhVien/src/ChuongTrinhChinh.java
@@ -0,0 +1,15 @@
+
+public class ChuongTrinhChinh {
+
+	public static void main(String[] args) {
+		 // Tạo mới 1 sinh viên
+		SinhVien sv1 = new SinhVien();
+		   // cấp/gán dữ liệu cụ thể
+		sv1.mssv="66112233";
+		sv1.ten="Phạm Thị Tươi Tắn Lắm";
+		sv1.tuoi=18;
+		sv1.hocBai("Lập trình Java");		
+
+	}
+
+}
---
+++ Vidu1_OOP_SinhVien/src/SinhVien.java
@@ -0,0 +1,11 @@
+public class SinhVien {
+	// Định nghĩa các thuộc tính
+	String ten;
+	String mssv;
+	int tuoi;
+	
+	// ĐỊnh nghĩa các phương thức/ hàm
+	void hocBai(String monhoc) {
+		System.out.print("Bạn " + ten + " đang học môn " + monhoc);
+	}
+}

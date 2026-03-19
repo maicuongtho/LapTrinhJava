580e934c67d921db49c2249747979b8a5a890268
Mai Cường Thọ <thomc@ntu.edu.vn>
2026-03-19 08:45:23 +0700

    Xong lớp Nhân viên văn phỏng + test

--- ViduOOP_Kethua_QLNV/src/thomc/quanlynhanvien/Run.java
+++ ViduOOP_Kethua_QLNV/src/thomc/quanlynhanvien/Run.java
@@ -6,7 +6,11 @@ public class Run {
 		// Test lớp nhân viên
 		NhanVien nv1 = new NhanVien("001", "Mai Cường Thọ", 2300000);
 //		nv1.hienThiThongTin();
-		System.out.print(nv1.toString());
+		System.out.println(nv1.toString());
+		
+		NhanVienVanPhong nvVanPhong1 = new NhanVienVanPhong("002", "Trần Thị Tươi Thắm", 2300000, 3, 200000);
+		System.out.println(nvVanPhong1.toString());
+//		nvVanPhong1.hienThiThongTin();
 	}
 
 }

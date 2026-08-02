75b274e980c4d7db9ba3beea5435876e6bf712e5
Mai Cường Thọ <thomc@ntu.edu.vn>
2026-08-02 09:03:54 +0700

    Hoàn thành bài

--- BaiHello/src/com/tiilaz/Chao.java
+++ BaiHello/src/com/tiilaz/Chao.java
@@ -3,7 +3,9 @@ package com.tiilaz;
 public class Chao {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.print("Chào CỦ CHI anh hùng!!");
 
 	}
 
